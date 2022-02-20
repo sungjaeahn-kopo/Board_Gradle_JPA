a960ef5ae25b0ec0c3eea93b80c17c2aa967e2ed
sungjaeahn-kopo <dtw421@naver.com>
2022-02-20 15:45:58 +0900

    [Board]Add a rendering a boardList

--- components/Boards.js
+++ components/Boards.js
@@ -1,56 +1,125 @@
-import { useEffect } from "react"
-import { getBoard } from "../services/BoardService"
+import { useEffect } from "react";
+import { getBoard } from "../services/BoardService";
+import { Table, Tag, Space } from "antd";
 
-const Boards = ({boards}) => {
+const Boards = ({ boards }) => {
+  const columns = [
+    {
+      title: "Index",
+      dataIndex: "seq",
+      key: "seq",
+    },
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Writer",
+      dataIndex: "writer",
+      key: "writer",
+    },
+    {
+      title: "Content",
+      dataIndex: "content",
+      key: "content",
+    },
+    {
+      title: "CreateDate",
+      dataIndex: "createDate",
+      key: "createDate",
+    },
+    {
+      title: "UpdateDate",
+      dataIndex: "updateDate",
+      key: "updateDate",
+    },
+    {
+      title: "Cnt",
+      dataIndex: "cnt",
+      key: "cnt",
+    },
+    // {
+    //   title: "Tags",
+    //   key: "tags",
+    //   dataIndex: "tags",
+    //   render: (tags) => (
+    //     <>
+    //       {tags.map((tag) => {
+    //         let color = tag.length > 5 ? "greekblue" : "green";
+    //         if (tag === "loser") {
+    //           color = "volano";
+    //         }
+    //         return (
+    //           <Tag color={color} key={tag}>
+    //             {tag.toUpperCase()}
+    //           </Tag>
+    //         );
+    //       })}
+    //     </>
+    //   ),
+    // },
+    // {
+    //   title: "Action",
+    //   key: "action",
+    //   render: (text, record) => (
+    //     <Space size="middle">
+    //       <a>Invite {record.name}</a>
+    //       <a>Delete</a>
+    //     </Space>
+    //   ),
+    // },
+  ];
 
-    console.log('boards length ::: ', boards.length);
-    if(boards.length === 0) return null
+  console.log("boards length ::: ", boards.length);
+  if (boards.length === 0) return null;
 
-    const onChange = () => {
-        
-    }
+  //   const onChange = () => {};
 
-    const BoardRow = (board, index) => {
-        return (
-            <tr key = {index} className={index%2 === 0? 'odd' : 'even'} onChange={onChange}>
-                <td>{index + 1}</td>
-                <td>{board.title}</td>
-                <td>{board.writer}</td>
-                <td>{board.content}</td>
-            </tr>
-        )
-    }
+  //   const BoardRow = (board, index) => {
+  //     return (
+  //       <tr
+  //         key={index}
+  //         className={index % 2 === 0 ? "odd" : "even"}
+  //         // onChange={onChange}
+  //       >
+  //         <td>{index + 1}</td>
+  //         <td>{board.title}</td>
+  //         <td>{board.writer}</td>
+  //         <td>{board.content}</td>
+  //       </tr>
+  //     );
+  //   };
 
-    const boardTable = boards.map((board, index) => BoardRow(board, index)) ;
+  //   const boardTable = boards.map((board, index) => BoardRow(board, index));
 
-        return (
-            <>
-                <h2>Boards</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Board Id</th>
-                            <th>Board Title</th>
-                            <th>Board Writer</th>
-                            <th>Board Content</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {boardTable}
-                    </tbody>
-                </table>
-            </>
-        )
-
-}
+  return (
+    // <>
+    //   <h2>Boards</h2>
+    //   <table>
+    //     <thead>
+    //       <tr>
+    //         <th>Board Id</th>
+    //         <th>Board Title</th>
+    //         <th>Board Writer</th>
+    //         <th>Board Content</th>
+    //       </tr>
+    //     </thead>
+    //     <tbody>{boardTable}</tbody>
+    //   </table>
+    // </>
+    <Table columns={columns} dataSource={boards} />
+  );
+};
 
 export default Boards;
 
 // export const Boards = ({boards}) => {
 //     console.log('boards length:::', boards.length)
-    
+
 //     if(boards.length === 0) return null
-    
+
 //     const BoardRow = (board, index) => {
 
 //         return (
