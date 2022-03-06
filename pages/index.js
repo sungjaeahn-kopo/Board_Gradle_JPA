149b0dbab5c629d9fbf72b349098c1666e8ca87e
sungjaeahn-kopo <dtw421@naver.com>
2022-03-06 11:50:25 +0900

    [Board] Add a ServerSideRendering(board)

--- pages/index.js
+++ pages/index.js
@@ -7,3 +7,12 @@ import { BrowserRouter } from "react-router-dom";
 export default function Home() {
   return <App />;
 }
+
+export async function getServerSideProps() {
+  const { board } = await (await fetch("/api/board")).json();
+  return {
+    props: {
+      board,
+    },
+  };
+}
