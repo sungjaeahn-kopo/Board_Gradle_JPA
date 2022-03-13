0ed4de7d1e852e758b006610a6892ab46a9bec5f
sungjaeahn-kopo <dtw421@naver.com>
2022-03-13 15:44:28 +0900

    [Board] Add a style of Statistics part

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,57 @@
 import HighchartsReact from "highcharts-react-official";
-import Highcharts from "highcharts";
+import Highcharts, { charts } from "highcharts";
 
 const options = {
+  chart: {
+    type: "areaspline",
+  },
   title: {
     text: "My chart",
+  },
+  legend: {
+    layout: "vertical",
+    align: "left",
+    verticalAlign: "top",
+    x: 150,
+    y: 100,
+    floating: true,
+    borderWidth: 1,
+  },
+  xAxis: {
+    categories: [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ],
+    plotBands: [
+      {
+        // visualize the weekend
+        from: 4.5,
+        to: 6.5,
+        color: "rgba(68, 170, 213, .2)",
+      },
+    ],
+  },
+  yAxis: {
+    title: {
+      text: "Fruit units",
+    },
+  },
+  tooltip: {
+    shared: true,
+    valueSuffix: " units",
+  },
+  credits: {
+    enabled: false,
+  },
+  plotOptions: {
+    areaspline: {
+      fillOpacity: 0.5,
+    },
   },
   series: [
     {
